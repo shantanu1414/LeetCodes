1e5def0637812a7657b31486f200a83b1983b82c
shantanu1414 <97077964+shantanu1414@users.noreply.github.com>
2024-05-23 15:13:31 +0530

    Time: 9 ms (79.25%) | Memory: 19.3 MB (96.18%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+    vector<char>::iterator it=(upper_bound(letters.begin(),letters.end(),target));
+    if(it==letters.end())
+    {
+        return letters.front();
+    }
+    else
+    {
+        return *it;
+    }
+    }
+};
