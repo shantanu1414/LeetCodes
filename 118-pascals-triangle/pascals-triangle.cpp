864e2fb77463cac9dbc2fdf6a827f8244ab7f093
shantanu1414 <97077964+shantanu1414@users.noreply.github.com>
2024-06-02 22:04:21 +0530

    Time: 4 ms (33.91%) | Memory: 7.9 MB (46.96%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
-    int value(int r,int c)
-        { 
+    // int value(int r,int c)
+    //     { 
             
-            int val=1;
-            for(int x=1;x<=c;x++)
-            {
-                val=val*(r-x+1)/x;
-            }
-            return val;
+    //         int val=1;
+    //         for(int x=1;x<=c;x++)
+    //         {
+    //             val=val*(r-x+1)/x;
+    //         }
+    //         cout<<val;
+    //         return val;
+    //     }
+
+    vector<int> generaterow(int row)
+    {
+        long long ans=1;
+        vector<int> rowans;
+        rowans.push_back(1);
+        for(int col=1;col<row;col++)
+        {
+            ans=ans*(row-col);
+            ans=ans/col;
+            rowans.push_back(ans);
         }
+        return rowans;
+    }
     vector<vector<int>> generate(int numRows) {
         
 
         vector<vector<int>> pascal;
-        for(int x=0;x<numRows;x++)
+        for(int x=1;x<=numRows;x++)
         {
-            vector <int> temp;
-            for(int y=0;y<=x;y++)
-            {
-                temp.push_back(value(x,y));
+            // vector <int> temp;
+            // int ans=1;
+            // temp.push_back(ans);
+            // for(int y=1;y<x;y++)
+            // {
+            //     ans*=(x-y)/(y);
+            //     temp.push_back(ans);
                
-            }
-            pascal.push_back(temp);
+            // }
+            pascal.push_back(generaterow(x));
         }
         return pascal;
     }
